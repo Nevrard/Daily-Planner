451ef91c067e7db20a72946e581ad538e59ae1e3
Nevrard <ndangaevrard@gmail.com>
2021-03-25 14:00:04 -0400

    uploading files

---
+++ Assets/js/script.js
@@ -0,0 +1,49 @@
+var  currentTime=moment().format("dddd,MMMM,Do")
+
+console.log(currentTime)
+$("#currentDay").text(currentTime);
+var saveEl=$(".saveBtn")
+var descriptionEl= $(".description")
+
+
+var Save= function(event){
+    //var
+    var hour = $(this).siblings('.hour').text().trim()
+    console.log(hour)
+    var ToDo = $(this).siblings('.description').val()
+    console.log(ToDo)
+    localStorage.setItem(hour,ToDo)
+
+}
+
+saveEl.on('click',Save)
+
+$('#9').val(localStorage.getItem('9AM'))
+$('#10').val(localStorage.getItem('10AM'))
+$('#11').val(localStorage.getItem('11AM'))
+$('#12').val(localStorage.getItem('12AM'))
+$('#13').val(localStorage.getItem('1PM'))
+$('#14').val(localStorage.getItem('2PM'))
+$('#15').val(localStorage.getItem('3PM'))
+$('#16').val(localStorage.getItem('4PM'))
+$('#17').val(localStorage.getItem('5PM'))
+
+
+
+function displayTimeBlocks(){
+    var nowTime= parseInt(moment().format("HH"))
+    console.log(nowTime)
+    var hash="#"
+    for (time = 9; time <= 17; time++) {
+        var idEl=hash+time
+         if (time < nowTime) {
+            $(idEl).addClass("past");
+        } else if (time == nowTime) {
+            $(idEl).addClass("present");
+        } else if (time > nowTime) {
+            $(idEl).addClass("future");
+        }
+    }  
+}
+
+displayTimeBlocks();
